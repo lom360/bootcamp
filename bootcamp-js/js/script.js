52d287f1f05a9cc49b340c38d1daca9dbad5fcd8
lom360 <lom360@mail.fresnostate.edu>
2018-01-18 20:59:00 -0800

    day2

--- bootcamp-js/js/script.js
+++ bootcamp-js/js/script.js
@@ -6,3 +6,24 @@ console.log(num);
 
 var liar = false;
 console.log(liar);
+
+var firstNumber = 22;
+var secondNumber = 11;
+
+console.log(something);
+var something = "nothing";
+
+const pleaseChange = "I don't change bro";
+//pleaseChange = "Change right now";   "const value cannot change";
+console.log(pleaseChange);
+
+
+let rightNow = Date();
+console.log(rightNow);
+
+
+var iLikePie = Math.PI;
+console.log(iLikePie);
+
+var string = "hahahaha";
+var newString = new String("hahahaha");
