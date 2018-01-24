a044df93794489d79c813f45c27aa732258ce6d8
lom360 <lom360@mail.fresnostate.edu>
2018-01-23 20:59:05 -0800

    converting string to int

--- bootcamp-js/js/script.js
+++ bootcamp-js/js/script.js
@@ -1,53 +1,16 @@
-var sentence = "Where's my cat";
-console.log(sentence);
+const bestNumber = 42;
 
-var num = 7;
-console.log(num);
+console.log(!bestNumber);
 
-var liar = false;
-console.log(liar);
-
-var firstNumber = 22;
-var secondNumber = 11;
-
-console.log(something);
-var something = "nothing";
-
-const pleaseChange = "I don't change bro";
-//pleaseChange = "Change right now";   "const value cannot change";
-console.log(pleaseChange);
+var myString = "Batman";
+var newString = new String(myString);
+console.log(myString);
+console.log(newString);
+console.log(myString == newString)
+console.log(typeof(myString));
+console.log(typeof(newString));
 
 
-let rightNow = Date();
-console.log(rightNow);
-
-
-var iLikePie = Math.PI;
-console.log(iLikePie);
-
-var string = "hahahaha";
-var newString = new String("hahahaha");
-
-
-var myArray = ["Hello World", 22, true, null];
-console.log(myArray);
-
-var myObject = {
-  myString: "Hello World", magicNumber: 42
-};
-
-
-// let helloMessage = alert("Hey, there! My book is only $9.99...");
-//
-//
-// let promptMessage = prompt("Tell me the password...","password");
-// console.log(promptMessage);
-
-
-let firstName = prompt("What is your first name?");
-let lastName  = prompt("What is your last name?");
-console.log(firstName);
-let myMessage = alert("Your last name is " + lastName);
-
-
-let birthday = prompt("What's your birhday?", "Please enter your birthday");
+var num1 = parseInt(prompt("Please enter an integer", 10));
+var num2 = parseInt(prompt("Please enter an integer", 10));
+alert(num1 + num2);
