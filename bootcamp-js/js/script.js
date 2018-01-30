eb3aceadcefcc7c5b8bff8cea78a3fa35925b8fb
lom360 <lom360@mail.fresnostate.edu>
2018-01-30 14:00:01 -0800

    Practicing arrays

--- bootcamp-js/js/script.js
+++ bootcamp-js/js/script.js
@@ -1,17 +1,27 @@
-var randomList = [];
-var starWarsFan = true;
+// var randomList = [];
+// var starWarsFan = true;
+//
+// randomList[0] = "Movies are boring";
+// randomList[1] = 7;
+// randomList[2] = !starWarsFan;
+// randomList[3] = 1990;
+//
+// console.log(randomList);
+// console.log(randomList.length);
+//
+// console.log(typeof(randomList));
+// console.log(Array.isArray(randomList));
+//
+// var bestTeacher = prompt('Who is the best teacher?', 'Oniziku');
+// randomList.unshift(bestTeacher);
+// console.log(randomList);
 
-randomList[0] = "Movies are boring";
-randomList[1] = 7;
-randomList[2] = !starWarsFan;
-randomList[3] = 1990;
+var movie1 = ["National Treasure", "Ben Gates"];
+var movie2 = ["Pirates of the Carribean", "Jack Sparrow"];
+var movie3 = ["Mad Max", "Furiosa"];
+var movie4 = ["The Dark Knight", "Batman"];
+var movie5 = ["Serenty", "Malcolm Reynolds"];
 
-console.log(randomList);
-console.log(randomList.length);
-
-console.log(typeof(randomList));
-console.log(Array.isArray(randomList));
-
-var bestTeacher = prompt('Who is the best teacher?', 'Oniziku');
-randomList.unshift(bestTeacher);
-console.log(randomList);
+var favoriteMovies = [movie1[0], movie2[0], movie3[0], movie4[0], movie5[0]];
+alert("My favorite movies are " + favoriteMovies);
+alert("Array length is " + favoriteMovies.length);
