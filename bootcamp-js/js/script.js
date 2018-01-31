29903f46bf67409633693b88741259b402bd6da0
lom360 <lom360@mail.fresnostate.edu>
2018-01-30 18:31:57 -0800

    Learned about ES6

--- bootcamp-js/js/script.js
+++ bootcamp-js/js/script.js
@@ -45,18 +45,33 @@ const bestMovie = 5;
 //     break;
 // }
 
-var userAge = prompt('How old are you?', '200');
-if(userAge > 18) {
-  if(userAge < 80) {
-    var userAnswer = prompt('Do you like starwars');
-    if(userAnswer == 'yes'){
-      alert('We can\'t be friends');
-    }else {
-      alert('Cool you are awesome');
-    }
-  }else {
-    alert('You\'re too old');
-  }
-}else{
-  alert('You\'re not old enough');
+// var userAge = prompt('How old are you?', '200');
+// if(userAge > 18) {
+//   if(userAge < 80) {
+//     var userAnswer = prompt('Do you like starwars');
+//     if(userAnswer == 'yes'){
+//       alert('We can\'t be friends');
+//     }else {
+//       alert('Cool you are awesome');
+//     }
+//   }else {
+//     alert('You\'re too old');
+//   }
+// }else{
+//   alert('You\'re not old enough');
+// }
+
+
+var userName = prompt('Give me your user name');
+var favColor = prompt("What is your favorite color?", "blue or yellow");
+
+switch(favColor.toLowerCase()) {
+    case 'blue':
+        alert(userName + " You're blue, da ba di da ba die...");
+        break;
+    case 'yellow':
+        alert("You are so yellow...");
+        break;
+    default:
+        alert("Please answer with a valid color.");
 }
