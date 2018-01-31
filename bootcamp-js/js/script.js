e0289c8c83424d1673eeb227ab1f9d57f92a3cb2
lom360 <lom360@mail.fresnostate.edu>
2018-01-30 20:34:06 -0800

    Practicing some loops

--- bootcamp-js/js/script.js
+++ bootcamp-js/js/script.js
@@ -16,21 +16,46 @@
 // randomList.unshift(bestTeacher);
 // console.log(randomList);
 
-var movie1 = ["National Treasure", "Ben Gates"];
-var movie2 = ["Pirates of the Carribean", "Jack Sparrow"];
-var movie3 = ["Mad Max", "Furiosa"];
-var movie4 = ["The Dark Knight", "Batman"];
-var movie5 = ["Serenty", "Malcolm Reynolds"];
+var movie1 = ["National Treasure", "Ben Gates", 2000];
+var movie2 = ["Pirates of the Carribean", "Jack Sparrow", 2005];
+var movie3 = ["Mad Max", "Furiosa", 2015];
+var movie4 = ["The Dark Knight", "Batman", 2007];
+var movie5 = ["Serenity", "Malcolm Reynolds", 2005];
 
 var favoriteMovies = [];
 favoriteMovies.push(movie1, movie2, movie3, movie4, movie5);
+//
+// var topMovieTitles = [];
+// topMovieTitles.push(favoriteMovies[0][0], favoriteMovies[1][0], favoriteMovies[2][0], favoriteMovies[3][0], favoriteMovies[4][0]);
+// alert("My favorite movies are " + topMovieTitles[0] + ", " + topMovieTitles[1] + ", " + topMovieTitles[2] + ", "  + topMovieTitles[3] + ", " + topMovieTitles[4]);
+//
+// var worseAndBest = [favoriteMovies[0][0], favoriteMovies[4][0]];
+//
+// console.log("The best movie ever is " + worseAndBest[0] + " and " + "the worst movie ever is " + worseAndBest[1]);
+//
+// alert("Array length is " + favoriteMovies.length);
 
+var n = 0;
+var topMovie = 0;
 var topMovieTitles = [];
-topMovieTitles.push(favoriteMovies[0][0], favoriteMovies[1][0], favoriteMovies[2][0], favoriteMovies[3][0], favoriteMovies[4][0]);
-alert("My favorite movies are " + topMovieTitles[0] + ", " + topMovieTitles[1] + ", " + topMovieTitles[2] + ", "  + topMovieTitles[3] + ", " + topMovieTitles[4]);
 
-var worseAndBest = [favoriteMovies[0][0], favoriteMovies[4][0]];
+// while(n < favoriteMovies.length) {
+//     topMoviesTitles.push(favoriteMovies[n][0]);
+//     n++;
+// }
 
-console.log("The best movie ever is " + worseAndBest[0] + " and " + "the worst movie ever is " + worseAndBest[1]);
+// while(topMovie < favoriteMovies.length) {
+//     if(favoriteMovies[topMovie][0] == 'Serenity'){
+//         alert('The best movie ever made is ' + favoriteMovies[topMovie][0] + ' where release date was ' + favoriteMovies[topMovie][2]);
+//         break;
+//     }
+//     topMovie++;
+// }
 
-alert("Array length is " + favoriteMovies.length);
+for(var i = 0; i <= favoriteMovies.length - 1; i++) {
+    console.log(i);
+    if(favoriteMovies[i][0] == 'National Treasure'){
+        alert('The best movie ever made is ' + favoriteMovies[i][0] + ' where release date was ' + favoriteMovies[i][2]);
+        // break;
+    }
+}
